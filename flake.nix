37b09421439d2810de2f0e8501e8ef2a6157cae1
drzzln <drzzln@protonmail.com>
2026-03-15 21:09:17 -0400

    refactor: use substrate repo-flake.nix universal builder
    Replaces bespoke flake.nix boilerplate with declarative config via
    substrate's repo-flake.nix. All build logic, devShell tooling, and
    meta attributes derived from (language, builder) dispatch.

--- flake.nix
+++ flake.nix
@@ -10,22 +10,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, substrate, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-      mkGoTool = (import "${substrate}/lib/go-tool.nix").mkGoTool;
-    in {
-      packages.default = mkGoTool pkgs {
-        pname = "akeyless-csi-provider";
-        version = "0.0.0-dev";
-        src = self;
-        vendorHash = "sha256-8if5uRCc9HCP3EMth7TjtLM81Exjp9yLBGlVndIp+2A=";
-        description = "Akeyless provider for the Kubernetes Secrets Store CSI Driver";
-        homepage = "https://github.com/pleme-io/akeyless-csi-provider";
-      };
-
-      devShells.default = pkgs.mkShellNoCC {
-        packages = with pkgs; [ go gopls gotools ];
-      };
-    });
+  outputs = inputs: (import "${inputs.substrate}/lib/repo-flake.nix" {
+    inherit (inputs) nixpkgs flake-utils;
+  }) {
+    self = inputs.self;
+    language = "go";
+    builder = "tool";
+    pname = "akeyless-csi-provider";
+    vendorHash = "sha256-8if5uRCc9HCP3EMth7TjtLM81Exjp9yLBGlVndIp+2A=";
+    description = "Akeyless provider for the Kubernetes Secrets Store CSI Driver";
+    homepage = "https://github.com/pleme-io/akeyless-csi-provider";
+  };
 }
